b7968af53902c15092f9048b469b1e51cb829a03
Patrick Dolan <dolanp1992@gmail.com>
2023-09-21 09:01:33 -0700

    remove react fragment

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -2,34 +2,32 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <>
-      <header>
-        <nav className="container row">
-          <div className="nav__left">
-            <h1 className="nav__logo">GG</h1>
-            <ul className="nav__list">
-              <li className="nav__list-item">
-                <Link className="nav__link" to={"/"}>
-                  Home
-                </Link>
-              </li>
-              <li className="nav__list-item">
-                <Link className="nav__link" to={"/games"}>
-                  Games
-                </Link>
-              </li>
-            </ul>
-          </div>
+    <header>
+      <nav className="container row">
+        <div className="nav__left">
+          <h1 className="nav__logo">GG</h1>
           <ul className="nav__list">
             <li className="nav__list-item">
-              <Link className="nav__link nav__link-button" to={"/account"}>
-                Account
+              <Link className="nav__link" to={"/"}>
+                Home
+              </Link>
+            </li>
+            <li className="nav__list-item">
+              <Link className="nav__link" to={"/games"}>
+                Games
               </Link>
             </li>
           </ul>
-        </nav>
-      </header>
-    </>
+        </div>
+        <ul className="nav__list">
+          <li className="nav__list-item">
+            <Link className="nav__link nav__link-button" to={"/account"}>
+              Account
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
