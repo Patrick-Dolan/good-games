09b1c560c95bdeb2ffa1f2ac55ec45c0dd33d874
Patrick Dolan <dolanp1992@gmail.com>
2023-09-28 08:25:22 -0700

    remove strictmode so hooks will stop running twice

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
-  </React.StrictMode>
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>
 );
