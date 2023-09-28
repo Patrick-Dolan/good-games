618eaff944231686d67ef7cb399d7ee31beba0b8
Patrick Dolan <dolanp1992@gmail.com>
2023-09-28 08:25:52 -0700

    create webpage components

---
+++ src/components/pages/AccountRegisterPage.tsx
@@ -0,0 +1,11 @@
+import RegisterAccount from '../account/RegisterAccount';
+
+function AccountRegisterPage() {
+  return (
+    <div className="container">
+      <RegisterAccount />
+    </div>
+  )
+}
+
+export default AccountRegisterPage;
