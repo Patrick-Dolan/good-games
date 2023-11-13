c8aab09f8440404e462e307b4584a54c3acc7df6
Patrick Dolan <dolanp1992@gmail.com>
2023-11-12 21:15:54 -0800

    add default profile picture to account page

--- src/components/account/Account.jsx
+++ src/components/account/Account.jsx
@@ -1,12 +1,18 @@
 import { useFirebaseAuth } from "../../context/AuthContext";
+import DefaultPFP from "./../../assets/DefaultPFP.png";
 
 function Account() {
   const { user } = useFirebaseAuth();
 
   return (
     <div className="container">
-      <h3>Account</h3>
+      <h1>Account</h1>
       <hr />
+      <img 
+        src={user?.photoURL ? user.photoURL : DefaultPFP} 
+        alt="A user profile picture or default if it hasn't been assigned" 
+        className="profile-picture"
+      />
       <p>Username: {user?.displayName}</p>
       <p>Email: {user?.email}</p>
     </div>
