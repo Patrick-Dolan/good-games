63c72387de2ae5b56036e1849f5c0b58c9d644db
Patrick Dolan <dolanp1992@gmail.com>
2023-11-12 19:41:44 -0800

    add basic homepage

--- src/components/home/Homepage.jsx
+++ src/components/home/Homepage.jsx
@@ -1,5 +1,13 @@
 function Homepage() {
-  return <div>Homepage Placeholder</div>;
+  return (
+    <div className="container">
+      <h1>GoodGames</h1>
+      <hr />
+      <p>Add splash images with basic summaries of ways to use site</p>
+      <p>Add featured game section</p>
+      <p>Add search and common tags section</p>
+    </div>
+  )
 }
 
 export default Homepage;
