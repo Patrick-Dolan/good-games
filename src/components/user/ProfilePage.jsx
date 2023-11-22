85d970da5ecaa3d4957e5187009e1e282a545e3c
Patrick Dolan <dolanp1992@gmail.com>
2023-11-22 09:13:48 -0800

    add basic profilepage component

---
+++ src/components/user/ProfilePage.jsx
@@ -0,0 +1,61 @@
+import { useParams } from "react-router";
+import Surface from "../layout/Surface";
+import { useEffect, useState } from "react";
+import { getUserData } from "../../../firestoreFunctions";
+import DefaultPFP from "./../../assets/DefaultPFP.png";
+
+
+function ProfilePage() {
+  const { userId } = useParams();
+  const [userProfileData, setUserProfileData] = useState(null);
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      const userData = await getUserData(userId);
+      setUserProfileData(userData);
+    }
+    getUserInfo();
+  }, []);
+
+  return (
+    !userProfileData
+    ? (
+      <div className="container">
+        <Surface elevation="elevation-1">
+          <h3 className="text-center">Profile Loading...</h3>
+        </Surface>
+      </div>
+    )
+    : (
+      <div className="container">
+        <div className="mb-1">
+          <Surface elevation="elevation-1">
+            <div>
+              <img
+                src={userProfileData?.photoURL ? userProfileData.photoURL : DefaultPFP}
+                alt="A user profile picture or default if it hasn't been assigned"
+                className="profile-picture margin-center"
+              />
+              <p className="text-center">@{userProfileData?.displayName}</p>
+              <hr />
+            </div>
+          </Surface>
+        </div>
+        <div className="mb-1">
+          <Surface elevation="elevation-1">
+            <h4>Favorite games shelf</h4>
+            <hr />
+            <p>*GAME THUMBNAILS GO HERE*</p>
+          </Surface>
+        </div>
+        <Surface elevation="elevation-1">
+          <h4>Games shelves</h4>
+          <hr />
+          <p>*SHELF LINKS HERE*</p>
+        </Surface>
+      </div>
+    )
+  )
+}
+
+export default ProfilePage;
